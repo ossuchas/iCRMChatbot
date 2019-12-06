fb27d80a32a65ac31671df3d18824296c419dfb2
ossuchas <suchat.s14102526@gmail.com>
2019-12-07 05:00:42 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from dotenv import load_dotenv
+from flask_cors import CORS
+from marshmallow import ValidationError
+
+from db import db
+from ma import ma
+
+app = Flask(__name__)
+
+api = Api(app, prefix="/api/v1")
+CORS(app, resources=r"/api/*", allow_headers="Content-Type")
+
+load_dotenv(".env", verbose=True)
+app.config.from_object("config")
+app.config.from_envvar("APPLICATION_SETTING")
+@app.route('/')
+def hello_world():
+    return 'AP Line Chat Bot Hello World! v2.0.1'
+
+if __name__ == '__main__':
+    db.init_app(app)
+    ma.init_app(app)
+    app.run(host="0.0.0.0", port=5000, debug=True)
